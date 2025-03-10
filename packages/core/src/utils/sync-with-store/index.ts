abc11cd5d4888d64c9897d435ea4efed30216209
Vaibhav Shinde <vaibhav@dyte.io>
2025-03-10 10:31:27 +0530

    fix: SyncWithStore logic

--- packages/core/src/utils/sync-with-store/index.ts
+++ packages/core/src/utils/sync-with-store/index.ts
@@ -3,29 +3,32 @@ import store, { addCallback, deleteCallback, type DyteUIStore } from './ui-store
 
 export function SyncWithStore() {
   return function (proto: ComponentInterface, propName: keyof DyteUIStore) {
-    let isUpdatingFromStore = false;
-    let onChangeCallback: (newValue: any) => void | undefined;
+    let onChangeCallback: any;
 
-    const { connectedCallback, componentShouldUpdate, disconnectedCallback } = proto;
+    type ValueType = DyteUIStore[keyof DyteUIStore];
+
+    const { connectedCallback, disconnectedCallback } = proto;
 
     proto.connectedCallback = function () {
       const host = getElement(this);
-      const value = host[propName];
+      const value = host[propName as string];
 
       if (!value) {
         const storeValue = store.state[propName];
 
         if (storeValue) {
-          // if no initial value, set it from store
-          isUpdatingFromStore = true;
-          host[propName] = storeValue;
-          isUpdatingFromStore = false;
+          host[propName as string] = storeValue;
         }
 
-        onChangeCallback = (newValue) => {
-          isUpdatingFromStore = true;
-          host[propName] = newValue;
-          isUpdatingFromStore = false;
+        onChangeCallback = (newValue: ValueType, oldValue?: ValueType) => {
+          const currentValue = host[propName as string];
+          if (currentValue !== oldValue) {
+            // remove callback since prop/state was changed manually
+            deleteCallback(propName, onChangeCallback);
+            onChangeCallback = undefined;
+            return;
+          }
+          host[propName as string] = newValue;
         };
 
         addCallback(propName, onChangeCallback);
@@ -34,18 +37,8 @@ export function SyncWithStore() {
       return connectedCallback?.call(this);
     };
 
-    proto.componentShouldUpdate = function (newVal, oldVal, prop: string) {
-      if (prop === propName && !isUpdatingFromStore) {
-        // if user updates prop after component init, delete callback
-        deleteCallback(propName, onChangeCallback);
-        onChangeCallback = undefined;
-      }
-      return componentShouldUpdate?.call(this, newVal, oldVal, prop);
-    };
-
     proto.disconnectedCallback = function () {
       deleteCallback(propName, onChangeCallback);
-      onChangeCallback = undefined;
       return disconnectedCallback?.call(this);
     };
   };
