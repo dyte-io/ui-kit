82a76e76ab88ca8e0656cbbfe600234003a813d3
Vaibhav Shinde <vaibhav@dyte.io>
2025-03-24 17:33:44 +0530

    fix: state updates in ui-provider

--- packages/core/src/components/dyte-ui-provider/dyte-ui-provider.tsx
+++ packages/core/src/components/dyte-ui-provider/dyte-ui-provider.tsx
@@ -1,13 +1,38 @@
-import { Component, Event, EventEmitter, h, Listen, Prop, State, Watch } from '@stencil/core';
-import { Meeting } from '../../types/dyte-client';
-import { DyteI18n, IconPack, States } from '../../exports';
+import {
+  Component,
+  Element,
+  Event,
+  EventEmitter,
+  h,
+  Listen,
+  Prop,
+  State,
+  Watch,
+} from '@stencil/core';
+import { Meeting, RoomLeftState } from '../../types/dyte-client';
+import {
+  defaultConfig,
+  DyteI18n,
+  generateConfig,
+  IconPack,
+  provideDyteDesignSystem,
+  Size,
+  States,
+  UIConfig,
+} from '../../exports';
 import { uiState, uiStore } from '../../utils/sync-with-store/ui-store';
 import deepMerge from 'lodash-es/merge';
+import { PermissionSettings } from '../../types/props';
+import { getSize } from '../../utils/size';
+
+const LEAVE_ROOM_TIMER = 10000;
 
 @Component({
   tag: 'dyte-ui-provider',
 })
 export class DyteUiProvider {
+  @Element() host: HTMLDyteUiProviderElement;
+
   /** Meeting */
   @Prop()
   meeting: Meeting;
@@ -20,6 +45,15 @@ export class DyteUiProvider {
   @Prop()
   t: DyteI18n;
 
+  /** Config */
+  @Prop() config: UIConfig;
+
+  /** Size */
+  @Prop({ reflect: true, mutable: true }) size: Size;
+
+  /** Whether to show setup screen or not */
+  @Prop() showSetupScreen: boolean = false;
+
   /**
    * Do not render children until meeting is initialized
    * @default false
@@ -30,6 +64,10 @@ export class DyteUiProvider {
 
   /** States */
   @Event({ eventName: 'dyteStatesUpdate' }) statesUpdate: EventEmitter<States>;
+
+  private authErrorListener: (ev: CustomEvent<Error>) => void;
+
+  private resizeObserver: ResizeObserver;
 
   @Listen('dyteStateUpdate')
   listenState(e: CustomEvent<States>) {
@@ -43,14 +81,65 @@ export class DyteUiProvider {
   }
 
   connectedCallback() {
+    if (typeof window !== 'undefined') {
+      this.authErrorListener = (ev) => {
+        if (ev.detail.message.includes('401')) {
+          this.updateStates({ meeting: 'ended', roomLeftState: 'unauthorized' });
+        }
+      };
+      window.addEventListener('dyteError', this.authErrorListener);
+    }
+
     this.onMeetingChange(this.meeting);
     this.onIconPackChange(this.iconPack);
     this.onTChange(this.t);
+    this.onConfigChange(this.config);
+    this.onSizeChange(this.size);
+
+    this.resizeObserver = new ResizeObserver(() => this.handleResize());
+    this.resizeObserver.observe(this.host);
+  }
+
+  disconnectedCallback() {
+    if (!this.meeting) return;
+    this.meeting.self.removeListener('roomLeft', this.roomLeftListener);
+    this.meeting.self.removeListener('roomJoined', this.roomJoinedListener);
+    this.meeting.self.removeListener('waitlisted', this.waitlistedListener);
+    this.meeting.self.removeListener('mediaPermissionUpdate', this.mediaPermissionUpdateListener);
+    this.meeting.meta.removeListener('socketConnectionUpdate', this.socketConnectionUpdateListener);
+    window.removeEventListener('dyteError', this.authErrorListener);
   }
 
   @Watch('meeting')
-  onMeetingChange(newMeeting: Meeting) {
-    uiStore.state.meeting = newMeeting;
+  onMeetingChange(meeting: Meeting) {
+    uiStore.state.meeting = meeting;
+
+    if (!meeting) return;
+
+    this.updateStates({ viewType: meeting.meta.viewType });
+    this.loadTheme();
+
+    meeting.self.addListener('roomJoined', this.roomJoinedListener);
+    meeting.self.addListener('waitlisted', this.waitlistedListener);
+    meeting.self.addListener('roomLeft', this.roomLeftListener);
+    meeting.self.addListener('mediaPermissionUpdate', this.mediaPermissionUpdateListener);
+    meeting.meta.addListener('socketConnectionUpdate', this.socketConnectionUpdateListener);
+
+    if (meeting.connectedMeetings.supportsConnectedMeetings) {
+      meeting.connectedMeetings.once('changingMeeting', this.handleChangingMeeting);
+    }
+
+    if (meeting.self.roomJoined) {
+      this.updateStates({ meeting: 'joined' });
+    } else {
+      if (this.showSetupScreen) {
+        this.updateStates({ meeting: 'setup' });
+      } else {
+        meeting.joinRoom();
+      }
+    }
+
+    window.removeEventListener('dyteError', this.authErrorListener);
   }
 
   @Watch('iconPack')
@@ -61,6 +150,78 @@ export class DyteUiProvider {
   @Watch('t')
   onTChange(newT: DyteI18n) {
     uiStore.state.t = newT;
+  }
+
+  @Watch('config')
+  onConfigChange(config: UIConfig) {
+    uiStore.state.config = config;
+  }
+
+  @Watch('size')
+  onSizeChange(newSize: Size) {
+    uiStore.state.size = newSize;
+  }
+
+  private handleResize() {
+    this.size = getSize(this.host.clientWidth);
+  }
+
+  private loadTheme = () => {
+    let config = this.config;
+    if (config === defaultConfig) {
+      const generatedConfig = generateConfig(this.meeting.self.config, this.meeting);
+      config = generatedConfig.config;
+    }
+
+    if (config.designTokens) {
+      provideDyteDesignSystem(document.documentElement, config.designTokens);
+    }
+  };
+
+  private roomJoinedListener = () => {
+    this.updateStates({ meeting: 'joined' });
+  };
+
+  private waitlistedListener = () => {
+    this.updateStates({ meeting: 'waiting' });
+  };
+
+  private roomLeftListener = ({ state }: { state: RoomLeftState }) => {
+    // Let socketConnectionUpdate listener handle this case.
+    if (state === 'disconnected' || state === 'failed') return;
+    this.updateStates({ meeting: 'ended', roomLeftState: state });
+  };
+
+  private mediaPermissionUpdateListener = ({ kind, message }) => {
+    if (['audio', 'video'].includes(kind)) {
+      if (
+        (message === 'DENIED' || message === 'SYSTEM_DENIED') &&
+        uiState.states.activeDebugger !== true
+      ) {
+        const permissionModalSettings: PermissionSettings = {
+          enabled: true,
+          kind,
+        };
+        this.updateStates({ activePermissionsMessage: permissionModalSettings });
+      }
+    }
+  };
+
+  private socketConnectionUpdateListener = ({ state }) => {
+    if (state === 'failed') {
+      setTimeout(() => {
+        this.meeting.leave('disconnected');
+      }, LEAVE_ROOM_TIMER);
+    }
+  };
+
+  private handleChangingMeeting(destinationMeetingId: string) {
+    this.updateStates({
+      activeBreakoutRoomsManager: {
+        ...uiState.states.activeBreakoutRoomsManager,
+        destinationMeetingId,
+      },
+    });
   }
 
   render() {
